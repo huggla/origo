04d5ff3683c6091be50df75e59276baa2eebdd25
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-06-19 12:23:26 +0200

    Update infowindow.js

--- src/infowindow.js
+++ src/infowindow.js
@@ -112,7 +112,7 @@ function createCloseButton() {
 function createMinimizeButton() {
   return Button({
     cls: 'minimizebutton-svg-container small round small icon-smaller grey-lightest margin-top-small margin-right-small z-index-ontop-low ',
-    icon: '#ic_close_24px',
+    icon: '#ic_minimize_24px',
     state: 'initial',
     validStates: ['initial', 'hidden'],
     ariaLabel: 'Minimera'
