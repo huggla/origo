6c8fd6725d26d82ae6801e864a38f74a5f221f8b
Henrik Uggla <henrik.uggla@kristianstad.se>
2024-10-09 12:31:59 +0200

    Update legendmaker.js

--- src/utils/legendmaker.js
+++ src/utils/legendmaker.js
@@ -152,7 +152,7 @@ function updateLayer(layer, viewer) {
       if (filterStr === '') {
         filterStr = "IN ( '' )";
       }
-      filterStr = str.replace(/\(([^ ])/g, "( $1").replace(/([^ ])\)/g, "$1 )");
+      filterStr = filterStr.replace(/\(([^ ])/g, "( $1").replace(/([^ ])\)/g, "$1 )");
       layer.getSource().updateParams({ FILTER: layer.get('name') + ':' + filterStr });
     } else {
       layer.getSource().updateParams({ FILTER: null });
