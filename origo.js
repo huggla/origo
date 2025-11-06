ad6b94ce51aaf99ed941913eca03fcfc199a1de2
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-11-06 19:32:03 +0100

    Update origo.js

--- origo.js
+++ origo.js
@@ -103,42 +103,25 @@ const Origo = function Origo(configPath, options = {}) {
   api.extensions = () => origoExtensions;
 
   /** Helper that initialises a new viewer  */
-const initViewer = () => {
-  const defaultConfig = Object.assign({}, origoConfig, options);
-  loadResources(configPath, defaultConfig)
-    .then(async (data) => {
-      const viewerOptions = data.options;
-      viewerOptions.controls = await initControls(viewerOptions.controls);
-      viewerOptions.extensions = initExtensions(viewerOptions.extensions || []);
-      return viewerOptions;
-    })
-    .then((viewerOptions) => {
-      const target = viewerOptions.target;
-      viewer = Viewer(target, viewerOptions);
+viewer.on('loaded', () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const mapStateId = urlParams.get('mapStateId');
 
-      viewer.on('loaded', () => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const mapStateId = urlParams.get('mapStateId');
-
-        if (mapStateId) {
-          permalink.readStateFromServer(mapStateId).then(rawState => {
-            if (rawState) {
-              // ANVÄND Utils.urlparser
-              const hashStr = Utils.urlparser.formatUrl(rawState);
-              const hashUrl = `#${hashStr}`;
-              const parsedState = permalink.parsePermalink(hashUrl);
-              if (parsedState) {
-                viewer.dispatch('changestate', parsedState);
-              }
-            }
-          }).catch(err => console.error('Restore failed:', err));
+  if (mapStateId) {
+    permalink.readStateFromServer(mapStateId).then(rawState => {
+      if (rawState) {
+        const hashStr = Utils.urlparser.formatUrl(rawState);
+        const hashUrl = `#${hashStr}`;
+        const parsedState = permalink.parsePermalink(hashUrl);
+        if (parsedState) {
+          viewer.dispatch('changestate', parsedState);
         }
+      }
+    }).catch(err => console.error('Restore failed:', err));
+  }
 
-        origo.dispatch('load', viewer);
-      });
-    })
-    .catch(error => console.error(error));
-};
+  origo.dispatch('load', viewer);
+});
   // Add a listener to handle a new sharemap when using hash format.
   window.addEventListener('hashchange', (ev) => {
     const newParams = permalink.parsePermalink(ev.newURL);
