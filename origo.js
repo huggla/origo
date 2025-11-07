841d41c33118cf2cdc30d52166478756ffa1dc2e
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-11-07 10:25:30 +0100

    Update origo.js

--- origo.js
+++ origo.js
@@ -19,7 +19,7 @@ import Style from './src/style';
 import featurelayer from './src/featurelayer';
 import getFeatureInfo from './src/getfeatureinfo';
 import getFeature from './src/getfeature';
-import * as Utils from './src/utils'; // Inkluderar urlparser
+import * as Utils from './src/utils';
 import dropdown from './src/dropdown';
 import { renderSvgIcon } from './src/utils/legendmaker';
 import SelectedItem from './src/models/SelectedItem';
@@ -27,8 +27,6 @@ import 'elm-pep';
 import 'pepjs';
 import 'drag-drop-touch';
 import permalink from './src/permalink/permalink';
-import * as Loader from './src/loading';
-import Spinner from './src/utils/spinner';
 
 const Origo = function Origo(configPath, options = {}) {
   /** Reference to the returned Component */
@@ -39,7 +37,6 @@ const Origo = function Origo(configPath, options = {}) {
     featureinfoOptions: {},
     crossDomain: true,
     target: '#app-wrapper',
-    keyboardEventTarget: document,
     svgSpritePath: 'css/svg/',
     svgSprites: ['fa-icons.svg', 'material-icons.svg', 'miscellaneous.svg', 'origo-icons.svg', 'custom.svg'],
     breakPoints: {
@@ -102,7 +99,7 @@ const Origo = function Origo(configPath, options = {}) {
   api.controls = () => origoControls;
   api.extensions = () => origoExtensions;
 
-  /** Helper that initialises a new viewer */
+  /** Helper that initialises a new viewer  */
   const initViewer = () => {
     const defaultConfig = Object.assign({}, origoConfig, options);
     loadResources(configPath, defaultConfig)
@@ -112,51 +109,20 @@ const Origo = function Origo(configPath, options = {}) {
         viewerOptions.extensions = initExtensions(viewerOptions.extensions || []);
         const target = viewerOptions.target;
         viewer = Viewer(target, viewerOptions);
-
-        // --- VÄNTA PÅ 'loaded' ---
         viewer.on('loaded', () => {
-          // --- HANTERA ?mapStateId= ---
-          const urlParams = new URLSearchParams(window.location.search);
-          const mapStateId = urlParams.get('mapStateId');
-
-          if (mapStateId) {
-            permalink.readStateFromServer(mapStateId).then(rawState => {
-              if (rawState) {
-                try {
-                  // ÅTERANVÄND permalink.serializeState – en enda källa
-                  const serialized = permalink.serializeState(rawState, viewer);
-                  const hashStr = Utils.urlparser.formatUrl(serialized);
-                  const hashUrl = `#${hashStr}`;
-                  const parsedState = permalink.parsePermalink(hashUrl);
-
-                  if (parsedState) {
-                    viewer.dispatch('changestate', parsedState);
-                    console.log('MapState återställt från ?mapStateId=', mapStateId);
-                  }
-                } catch (err) {
-                  console.error('Restore failed:', err);
-                }
-              }
-            }).catch(err => {
-              console.error('Restore failed:', err);
-            });
-          }
-
-          // --- Trigga 'load' för extensions ---
+          // Inform listeners that there is a new Viewer in town
           origo.dispatch('load', viewer);
         });
       })
-      .catch(error => {
-        console.error('Failed to load config:', error);
-        renderError(error);
-      });
+      .catch(error => console.error(error));
   };
-
-  // --- HANTERA HASHCHANGE (för #center=... format) ---
+  // Add a listener to handle a new sharemap when using hash format.
   window.addEventListener('hashchange', (ev) => {
     const newParams = permalink.parsePermalink(ev.newURL);
-    if (newParams && newParams.map) {
-      initViewer(); // "Reboot" med nytt state
+
+    if (newParams.map) {
+      // "Reboot" the application by creating a new viewer instance using the original configuration and the new sharemap state
+      initViewer();
     }
   });
 
@@ -166,7 +132,7 @@ const Origo = function Origo(configPath, options = {}) {
     onInit() {
       const defaultConfig = Object.assign({}, origoConfig, options);
       const base = document.createElement('base');
-      base.href = defaultConfig.baseUrl || '/';
+      base.href = defaultConfig.baseUrl;
       document.getElementsByTagName('head')[0].appendChild(base);
       origo = this;
       initViewer();
@@ -174,12 +140,9 @@ const Origo = function Origo(configPath, options = {}) {
   });
 };
 
-// --- OpenLayers helpers ---
 olInteraction.Draw.createBox = createBox;
 olGeom.Polygon.fromCircle = fromCircle;
 olGeom.Polygon.fromExtent = fromExtent;
-
-// --- Exportera globalt ---
 Origo.controls = origoControls;
 Origo.extensions = origoExtensions;
 Origo.ui = ui;
@@ -201,10 +164,5 @@ Origo.Utils = Utils;
 Origo.dropdown = dropdown;
 Origo.renderSvgIcon = renderSvgIcon;
 Origo.SelectedItem = SelectedItem;
-Origo.Loader = {};
-Origo.Loader.show = Loader.showLoading;
-Origo.Loader.hide = Loader.hideLoading;
-Origo.Loader.withLoading = Loader.withLoading;
-Origo.Loader.getInlineSpinner = Spinner;
 
 export default Origo;
