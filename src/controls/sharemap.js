35b487266d76fc5ff2aaab117ad93b8f05625225
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-11-28 16:22:57 +0100

    Update sharemap.js

--- src/controls/sharemap.js
+++ src/controls/sharemap.js
@@ -10,6 +10,7 @@ const ShareMap = function ShareMap(options = {}) {
     icon = '#ic_screen_share_outline_24px',
     title = 'Dela karta',
     storeMethod,
+    loadMapStateIdMethod,
     serviceEndpoint
   } = options;
   let viewer;
@@ -38,6 +39,9 @@ const ShareMap = function ShareMap(options = {}) {
     onInit() {
       if (storeMethod && serviceEndpoint) {
         permalink.setSaveOnServerServiceEndpoint(serviceEndpoint);
+        if (loadMapStateIdMethod) {
+          permalink.setLoadMapStateIdMethod(loadMapStateIdMethod);
+        }
       }
     },
     onAdd(evt) {
