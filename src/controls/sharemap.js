e65474a04fb91c6e2220673251cce7941c4b41ac
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-11-28 16:16:22 +0100

    Update sharemap.js

--- src/controls/sharemap.js
+++ src/controls/sharemap.js
@@ -7,9 +7,18 @@ const ShareMap = function ShareMap(options = {}) {
   } = options;
 
   const {
+    localization
+  } = options;
+
+  function localize(key) {
+    return localization.getStringByKeys({ targetParentKey: 'sharemap', targetKey: key });
+  }
+
+  const {
     icon = '#ic_screen_share_outline_24px',
-    title = 'Dela karta',
+    title = localize('title'),
     storeMethod,
+	loadMapStateIdMethod,
     serviceEndpoint
   } = options;
   let viewer;
@@ -17,9 +26,9 @@ const ShareMap = function ShareMap(options = {}) {
   let menuItem;
   let modal;
 
-  const createContent = function createContent() {
-    return '<div class="o-share-link"><input type="text"></div>'
-    + '<i>Kopiera och klistra in länken för att dela kartan.</i>';
+  const createContent = function createContent() { // Kopiera och klistra in länken för att dela kartan.
+    const shareMapInstruction = localize('shareMapInstruction');
+    return `<div class="o-share-link"><input type="text"></div><i>${shareMapInstruction}</i>`;
   };
 
   const createLink = function createLink(data) {
@@ -38,6 +47,9 @@ const ShareMap = function ShareMap(options = {}) {
     onInit() {
       if (storeMethod && serviceEndpoint) {
         permalink.setSaveOnServerServiceEndpoint(serviceEndpoint);
+		if (loadMapStateIdMethod) {
+          permalink.setLoadMapStateIdMethod(loadMapStateIdMethod);
+        }
       }
     },
     onAdd(evt) {
@@ -48,7 +60,7 @@ const ShareMap = function ShareMap(options = {}) {
         click() {
           mapMenu.close();
           modal = Modal({
-            title: 'Länk till karta',
+            title: localize('linkToMap'),
             content: createContent(),
             target
           });
